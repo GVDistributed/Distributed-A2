448b4a79a5d1645b9e231293001a92f5563afa22
Aaron Voelker <dr.sane@gmail.com>
2012-03-09 17:25:26 -0500

    Router starting point

--- router.py
+++ router.py
@@ -49,26 +49,42 @@ class RoutingTable(object):
         return was_replaced
 
     def get_region(self, node):
-        rel_id = (node.int_id - self.node.int_id) % (1 << self.logm)
+        diff_bits = self.node.int_id ^ node.int_id
         for r in range(self.regions - 1):
-            if rel_id & (1 << (self.logm - r - 1)):
+            mask = (1 << (self.logm - r - 1))
+            if diff_bits & mask:
                 return r
         return self.regions - 1
 
+    def get_nodes(self):
+        return self.table.values()
+
     def debug(self):
+        print "[%x]" % self.node.int_id
         for r in range(self.regions):
             if self.table.has_key(r):
                 print "%d %032x" % (r, self.table[r].int_id)
             else:
                 print r, "None"
 
+def cw_distance(origin, p, mod):
+    return (p.int_id - origin.int_id) % mod
+
+def ccw_distance(origin, p, mod):
+    return (origin.int_id - p.int_id) % mod
+
+def distance(p1, p2, mod):
+    return min(cw_distance(p1, p2, mod), 
+               ccw_distance(p1, p2, mod))
+
 class NeighborSet(object):
     
-    def __init__(self, node, size):
+    def __init__(self, node, size, m):
         self.node = node
         self.size = size
-        self.ccw = []
         self.cw = []
+        self.ccw = []
+        self.m = m
 
     def is_neighbor(self, node):
         for neighbor in itertools.chain(self.cw, self.ccw):
@@ -76,31 +92,48 @@ class NeighborSet(object):
                  return True
         return False
         
-    def get_neighbors(self):
-        return self.ccw + self.cw
+    def cw_distance(self, node):
+        return cw_distance(self.node, node, self.m)
 
-    '''
+    def ccw_distance(self, node):
+        return ccw_distance(self.node, node, self.m)
+    
+    def get_neighbors(self):
+        return self.cw + self.ccw
+
+    def get_successor(self):
+        if not self.cw:
+            return None
+        return self.cw[0]
+
+    def get_predecessor(self):
+        if not self.ccw:
+            return None
+        return self.ccw[0]
+
     def update(self, nodes):
-        for node in nodes:
-            self.add_neighbor(node)
-            
-    def add_neighbor(self, node):
-        if node.int_id > self.node.int_id:
-            lis = self.cw
-        else:
-            lis = self.ccw
-        lis.append(node)
-        lis.sort(key = lambda x: x.int_id )
-        if len(lis) > self.size:
-            lis.pop()    
-    '''
-        
+        self.cw.extend(nodes)
+        self.ccw.extend(nodes)
+        self.cw.sort(key = self.cw_distance)
+        self.ccw.sort(key = self.ccw_distance)
+        self.cw = self.cw[:self.size]
+        self.ccw = self.ccw[:self.size]
+ 
     def debug(self):
-        print "CCW:", ', '.join("%032x" % node.int_id for node in reversed(self.ccw))
         print "CW: ", ', '.join("%032x" % node.int_id for node in self.cw)
+        print "CCW:", ', '.join("%032x" % node.int_id for node in self.ccw)
 
 class Router(object):
-    pass
+    
+    def __init__(self, node, n = 128):
+        self.n = n 
+        self.routing_table = RoutingTable(node, self.n, self.n / 32)
+        self.neighbor_set = NeighborSet(node, 2, 2**self.n)
+        self.node = node
+
+    def closest_node(self, node):
+        return min(self.routing_table.get_nodes() + self.neighbor_set.get_neighbors(),
+                   key = lambda p: distance(node, p, 2**self.n))
 
 if __name__ == '__main__':
 
@@ -112,16 +145,24 @@ if __name__ == '__main__':
     m5 = NodeID.dummy()
     m6 = NodeID.dummy()
     
-    #m0.id = struct.pack(">QQ", 0, 0)
-    
-    routing_table = RoutingTable(m0, 128, 128 / 32)
-    routing_table.update([m1, m2, m3, m4, m5, m6])
+    m0.id = struct.pack(">QQ", 0, 0)
+   
+    router = Router(m0)
 
+    routing_table = router.routing_table
+    routing_table.update([m1, m2, m3, m4, m5, m6])
     routing_table.debug()
 
-    neighbor_set = NeighborSet(m0, 2)
+    neighbor_set = router.neighbor_set
     
-    neighbor_set.update([m1, m2, m3])
-    print neighbor_set.debug()
-    neighbor_set.update([m1, m2, m3])
-    print neighbor_set.debug()
+    neighbor_set.update([m1])
+    neighbor_set.debug()
+    neighbor_set.update([m2, m3])
+    neighbor_set.debug()
+    neighbor_set.update([m4, m5, m6])
+    neighbor_set.debug()
+
+    for i in range(10):
+        m = NodeID.dummy()
+        print "%032x %032x" % (m.int_id, router.closest_node(m).int_id)
+
