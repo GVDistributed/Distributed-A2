2c6ef2969b4e9908fd7b1cce95975d3aa31fc7db
ggurugan <ggurugan@student.cs.uwaterloo.ca>
2012-03-09 18:42:28 -0500

    Wrapping the Client to provide a better interface

--- WatDHT/WatClient.py
+++ WatDHT/WatClient.py
@@ -1,40 +1,40 @@
-#!/usr/bin/env python
- 
 import sys
-sys.path.append('../gen-py')
-sys.path.append('/u5/gguruganesh/cs454/Distributed-A2/lib/python2.6/site-packages/')
- 
+
 from WatDHT import Client
 from constants import *
 from ttypes import *
- 
+
 from thrift import Thrift
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
- 
-try:
-  # Make socket
-  transport = TSocket.TSocket('localhost', 9090)
- 
-  # Buffering is critical. Raw sockets are very slow
-  transport = TTransport.TBufferedTransport(transport)
- 
-  # Wrap in a protocol
-  protocol = TBinaryProtocol.TBinaryProtocol(transport)
- 
-  # Create a client to use the protocol encoder
-  client = Client(protocol)
- 
-  # Connect!
-  transport.open()  
-  print("Going to ping") 
-  for i in xrange(1000000):
-    s= client.get(sys.argv[1])
-    print(s)
-  print("Done pinging")
 
-  transport.close()
- 
-except Thrift.TException, tx:
-  print "%s" % (tx.message)
+class WDHTClient():
+    def __init__(self, ip='localhost', port=9090):
+        self.ip = ip
+        self.port = port
+
+    def __getattr__(self,name):
+        # Make socket
+        transport = TSocket.TSocket(self.ip, self.port)
+        # Buffering is critical. Raw sockets are very slow
+        transport = TTransport.TBufferedTransport(transport)
+        # Wrap in a protocol
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+        # Create a client to use the protocol encoder
+        client = Client(protocol)
+        attr = getattr(client, name)
+        if callable(attr):
+            def wrapped(*args, **kwargs):
+                # Connect!
+                transport.open()
+                retval = attr(*args, **kwargs)
+                transport.close()
+                return retval
+            return wrapped
+        else:
+            return attr
+
+if __name__ == "__main__":
+    C = WDHTClient()
+    C.ping()
